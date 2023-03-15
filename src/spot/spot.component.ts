3616e6557d4e630683db2b6db2958e2d4020f5b7
Shuki Brightstein <Shuki.Brightstein@tibaparking.com>
2023-03-15 14:33:43 +0200

    dsdsd

--- src/spot/spot.component.ts
+++ src/spot/spot.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 import { ActivatedRoute } from "@angular/router";
 import { SpotDto } from "src/shared/entities/spot-dto";
 // import * as html2pdf from 'html2pdf.js';
@@ -12,14 +13,16 @@ import { SpotDto } from "src/shared/entities/spot-dto";
 export class SpotComponent implements OnInit {
 
   selectedSpot: SpotDto;
+  url: SafeUrl;
 
-  constructor(private activeRoute: ActivatedRoute) {
+  constructor(private activeRoute: ActivatedRoute, private domSanitizer: DomSanitizer) {
 
   }
 
   ngOnInit(): void {
     const spotId = ''+this.activeRoute.snapshot.paramMap.get('spotId');
     this.loadSelectedSpot(+spotId);
+    this.url = this.domSanitizer.bypassSecurityTrustUrl(`whatsapp://send?text=Your can find your spot here: https://scan-2-find.herokuapp.com/spot/${spotId}`)
   }
 
   loadSelectedSpot(spotId: number) {
@@ -34,7 +37,8 @@ export class SpotComponent implements OnInit {
   }
 
   onShareClick() {
-
+    const waSpotElement = document.getElementById('wa');
+    waSpotElement?.click();
   }
 
   onDownloadClick() {
